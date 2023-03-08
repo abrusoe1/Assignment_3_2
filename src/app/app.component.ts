8103da4033725316dbab2b6c5eaf80067af2a296
Andrew <abrusoe1@gmail.com>
2023-03-08 18:32:37 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormControl } from '@angular/forms';
+import { Todo } from 'src/Todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  tasks:Todo[] = [{task: "wake up", completed: true}, {task: "eat breakfast", completed: true}, {task: "shower", completed: false}, {task: "take a dump", completed: true}, {task: "brush teeth", completed: false}];
+
+  completeTask(i: number):void{
+    this.tasks[i].completed = true;
+  }
+
+  //The next three lines are commented out so that ng serve -o will actually compile
+  // addTask():void{
+  //   this.tasks.push({task: this.newTask, completed: false});
+  // }
+
+  removeTask(i: number):void{
+    this.tasks.splice(i, 1);
+  }
 }
